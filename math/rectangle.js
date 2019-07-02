a9d1b0876808b31b9943be8bcc8023f249aeec47
MyrPasko <myroslavpasko@gmail.com>
2019-07-02 22:22:22 +0300

    On lesson 75

---
+++ math/rectangle.js
@@ -0,0 +1,14 @@
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    function calcRectangle(height, length) {
+        return height * length;
+    }
+    exports_1("calcRectangle", calcRectangle);
+    return {
+        setters: [],
+        execute: function () {
+        }
+    };
+});
+//# sourceMappingURL=rectangle.js.map
