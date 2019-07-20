26ad2f23c9f2bdfbf3d862d28b62e9d82084768a
MyrPasko <myroslavpasko@gmail.com>
2019-07-20 22:36:23 +0300

    On lesson 124

--- src/decorators/decorators.js
+++ src/decorators/decorators.js
@@ -1,115 +1,103 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
-function logged(constructorFn) {
-    console.log('[Constructor]', constructorFn);
-}
-var Person = /** @class */ (function () {
-    function Person(name) {
-        this.name = name;
-    }
-    Person = __decorate([
-        logged
-    ], Person);
-    return Person;
-}());
-// Factory
-function logging(value) {
-    return value ? logged : null;
-}
-// @logging(true)
-var Car = /** @class */ (function () {
-    function Car() {
-    }
-    return Car;
-}());
-// Advanced
-function printable(constructorFn) {
-    constructorFn.prototype.print = function () {
-        console.log(this);
-    };
-}
-var Plant = /** @class */ (function () {
-    function Plant() {
-        this.name = "Green plant";
-    }
-    Plant = __decorate([
-        logged,
-        printable
-    ], Plant);
-    return Plant;
-}());
-var plant = new Plant();
-plant.print();
-// Method decorator
-// Property decorator
-function editable(value) {
-    return function (_target, _propName, descriptor) {
-        descriptor.writable = value;
-    };
-}
-function overwritable(value) {
-    return function (_target, _propName) {
-        var newDescriptor = {
-            writable: value,
-        };
-        return newDescriptor;
-    };
-}
-var Project = /** @class */ (function () {
-    function Project(name) {
-        this.name = name;
-        this.projectName = name;
-    }
-    Project.prototype.calcBudget = function () {
-        console.log('[Budget]', 1000);
-    };
-    __decorate([
-        overwritable(true)
-    ], Project.prototype, "projectName", void 0);
-    __decorate([
-        editable(true)
-    ], Project.prototype, "calcBudget", null);
-    return Project;
-}());
-var project = new Project('Super project');
-project.calcBudget();
-project.calcBudget = function () {
-    console.log('[Budget]', 2000);
-};
-project.calcBudget();
-// Parameter Decorator
-function printInfo(target, methodName, paramIndex) {
-    console.log('[Target: ]', target);
-    console.log('[Method name: ]', methodName);
-    console.log('[Parameter index: ]', paramIndex);
-}
-var Course = /** @class */ (function () {
-    function Course(name) {
-        this.name = name;
-        this.name = name;
-    }
-    Course.prototype.printStudentsNumber = function (_mode, printAll) {
-        if (printAll) {
-            console.log('[All]', 10000);
-        }
-        else {
-            console.log('[Not all]', 2000);
-        }
-    };
-    __decorate([
-        __param(0, printInfo), __param(1, printInfo)
-    ], Course.prototype, "printStudentsNumber", null);
-    return Course;
-}());
-var course = new Course('Super course');
-course.printStudentsNumber('All students', true);
-course.printStudentsNumber('Not all students', false);
+// function logged(constructorFn: Function): void {
+//   console.log('[Constructor]', constructorFn);
+// }
+//
+// @logged
+// class Person {
+//   constructor(public name: string) {
+//   }
+// }
+//
+// // Factory
+// function logging(value: boolean) {
+//   return value ? logged : null;
+// }
+//
+// // @logging(true)
+// class Car {
+//
+// }
+//
+// // Advanced
+//
+// function printable(constructorFn: Function) {
+//   constructorFn.prototype.print = function () {
+//     console.log(this);
+//   }
+// }
+//
+// @logged
+// @printable
+// class Plant {
+//   name = "Green plant";
+// }
+//
+// const plant = new Plant();
+// (<any>plant).print();
+//
+// // Method decorator
+// // Property decorator
+//
+// function editable(value: boolean) {
+//   return function (_target: any, _propName: string, descriptor: PropertyDescriptor) {
+//     descriptor.writable = value;
+//   }
+// }
+//
+// function overwritable(value: boolean) {
+//   return function (_target: any, _propName: string): any {
+//     const newDescriptor: PropertyDescriptor = {
+//       writable: value,
+//     };
+//
+//     return newDescriptor;
+//   }
+// }
+//
+// class Project {
+//   @overwritable(true)
+//   projectName: string;
+//
+//   constructor(public name: string) {
+//     this.projectName = name;
+//   }
+//
+//   @editable(true)
+//   calcBudget() {
+//     console.log('[Budget]', 1000);
+//   }
+// }
+//
+// const project = new Project('Super project');
+// project.calcBudget();
+// project.calcBudget = function () {
+//   console.log('[Budget]', 2000);
+// };
+// project.calcBudget();
+//
+// // Parameter Decorator
+// function printInfo(target: any, methodName: string, paramIndex: number) {
+//   console.log('[Target: ]', target);
+//   console.log('[Method name: ]', methodName);
+//   console.log('[Parameter index: ]', paramIndex);
+// }
+//
+// class Course {
+//   constructor(public name: string) {
+//     this.name = name;
+//   }
+//
+//   printStudentsNumber(@printInfo _mode: string, @printInfo printAll: boolean) {
+//     if (printAll) {
+//       console.log('[All]', 10000);
+//     } else {
+//       console.log('[Not all]', 2000);
+//     }
+//   }
+// }
+//
+// const course = new Course('Super course');
+// course.printStudentsNumber('All students', true);
+// course.printStudentsNumber('Not all students', false);
 //# sourceMappingURL=decorators.js.map
